bfd1c33ecbd99ef13c5a885414d8c261cefcc4ae
Iqbal Muhammad Fajar Nuralam <iqbalmfn@gmail.com>
2024-09-03 10:37:30 +0700

    fix: direct route root path

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Auth/Login');
+    return redirect()->route('login');
 });
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/dashboard', DashboardController::class)->name('dashboard');
