6b94be2100250b28453b821ac3c8a812ff69b8a7
Iqbal Muhammad Fajar Nuralam <iqbalmfn@gmail.com>
2024-09-23 14:32:35 +0700

    update version di footer

--- resources/js/Layouts/Partials/Footer.jsx
+++ resources/js/Layouts/Partials/Footer.jsx
@@ -33,7 +33,7 @@ const Footer = () => {
                 >
                     {props.app.name}
                 </a>
-                , All rights Reserved &copy; 2024 - v(1.0.0)
+                , All rights Reserved &copy; 2024 - v(2.2.0)
             </div>
         </footer>
     );
